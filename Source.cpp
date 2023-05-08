5f3ef96af60aad8262c598ed9bacf1e638ed7f8a
= <=>
2023-05-08 23:21:21 +0400

    i added header2.h file to source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 void main() {
 	cout << "Hello my world !  " << endl;
-	cout << "i added header file and this l - value to git - hub : ) " << endl;
+	cout << "i added header file and this l - value to git - hub : ) no 2 " << endl;
 }
